f9975a1b1da6fefa6c06248a5754040653021c68
n4hhh <thanh19112004@gmail.com>
2026-09-15 18:42:01 +0700

    feat: add Tavily travel search tool

--- starter_v0/tools/__init__.py
+++ starter_v0/tools/__init__.py
@@ -14,6 +14,7 @@ from .lookup_user.tool import lookup_user
 from .policy.tool import search_company_policy
 from .search_kb.tool import search_kb
 from .search_device_info.tool import search_device_info
+from .search_travel_info.tool import search_travel_info
 
 
 # These names are part of the fixed evaluation contract. Keep built-in names
@@ -24,6 +25,7 @@ TOOL_FUNCTIONS = {
     "clarify": ask_user,
     "search_kb": search_kb,
     "search_device_info": search_device_info,
+    "search_travel_info": search_travel_info,
     "check_service_status": check_service_status,
     "inspect_device": inspect_device,
     "lookup_user": lookup_user,
